0b36c79b0cca200d6f49dba21a4970e77677e394
Sidney Kochman <sidney@kochman.org>
2020-02-10 00:10:10 -0800

    transactions

--- backends/gcs/backend.go
+++ backends/gcs/backend.go
@@ -5,9 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"sort"
 	"strconv"
+	"strings"
+	"time"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -37,11 +42,18 @@ func NewBackend(bucket string) (*Backend, error) {
 func (b *Backend) New(id string, size, blockSize uint64) (*Handle, error) {
 	ctx := context.Background()
 
+	// make sure this doesn't exist
+	obj := b.b.Object(id + "/manifest.json")
+	_, err := obj.Attrs(ctx)
+	if err != storage.ErrObjectNotExist {
+		return nil, fmt.Errorf("manifest already exists")
+	}
+
 	// write a manifest with some info about this file
-	mw := b.b.Object(id + "/manifest.json").NewWriter(ctx)
+	mw := obj.NewWriter(ctx)
 	m := manifest{Size: size, BlockSize: blockSize}
 	enc := json.NewEncoder(mw)
-	err := enc.Encode(m)
+	err = enc.Encode(m)
 	if err != nil {
 		return nil, fmt.Errorf("unable to write manifest: %w", err)
 	}
@@ -82,14 +94,53 @@ func (b *Backend) Open(id string) (*Handle, error) {
 		size:      m.Size,
 		blockSize: m.BlockSize,
 	}
+
+	err = h.finalizeTransactions()
+	if err != nil {
+		return nil, fmt.Errorf("unable to finalize transactions: %w", err)
+	}
+
 	return h, nil
 }
 
+// Delete is on the backend instead of the Handle because it should be possible
+// to ensure files are deleted without acquiring a handle.
+func (b *Backend) Delete(id string) error {
+	ctx := context.Background()
+
+	// make sure this doesn't exist
+	q := &storage.Query{Prefix: id}
+	err := q.SetAttrSelection([]string{"Name"})
+	if err != nil {
+		return fmt.Errorf("unable to set attribute selection: %w", err)
+	}
+	it := b.b.Objects(ctx, q)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return fmt.Errorf("unable to iterate: %w", err)
+		}
+
+		obj := b.b.Object(attrs.Name)
+		err = obj.Delete(ctx)
+		if err != nil {
+			return fmt.Errorf("unable to delete [%s]", obj.ObjectName())
+		}
+	}
+	return nil
+}
+
 type Handle struct {
-	b         *storage.BucketHandle
 	prefix    string
 	size      uint64
 	blockSize uint64
+
+	b *storage.BucketHandle
+
+	// TODO: make sure this never wraps
+	txn uint64
 }
 
 type manifest struct {
@@ -97,16 +148,143 @@ type manifest struct {
 	BlockSize uint64
 }
 
+type transaction struct {
+	// transactions have a band so that any read against that band can quickly
+	// determine if it has an in-flight transaction
+	band uint64
+
+	// id is a monotonically-increasing number so that transactions are applied
+	// to bands in order. it comes from `Handle.txn`
+	id uint64
+
+	// offset is within the band
+	offset uint64
+
+	// key is the full key for the transaction object in the bucket
+	key string
+}
+
+// GCS only wants one write/obj/sec
+var bandRateLimiter = map[uint64]func(){}
+
+func (h *Handle) finalizeTransactions() error {
+	ctx := context.Background()
+
+	// discover any in-flight transactions
+	txnPrefix := h.prefix + "/txns/"
+	q := &storage.Query{Prefix: txnPrefix}
+	err := q.SetAttrSelection([]string{"Name"})
+	if err != nil {
+		return fmt.Errorf("unable to set attribute selection: %w", err)
+	}
+
+	txns := []transaction{}
+	it := h.b.Objects(ctx, q)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return fmt.Errorf("unable to iterate: %w", err)
+		}
+
+		key := attrs.Name
+
+		split := strings.Split(key[len(txnPrefix):], "-")
+		if len(split) != 3 {
+			return fmt.Errorf("malformed transaction key")
+		}
+
+		band, err := strconv.ParseUint(split[0], 36, 64)
+		if err != nil {
+			return fmt.Errorf("unable to parse band: %w", err)
+		}
+
+		txn, err := strconv.ParseUint(split[1], 10, 64)
+		if err != nil {
+			return fmt.Errorf("unable to parse transaction ID: %w", err)
+		}
+
+		offset, err := strconv.ParseUint(split[2], 10, 64)
+		if err != nil {
+			return fmt.Errorf("unable to parse offset: %w", err)
+		}
+
+		t := transaction{
+			band:   band,
+			id:     txn,
+			offset: offset,
+			key:    key,
+		}
+		txns = append(txns, t)
+	}
+	// we need to put these in numeric order by txn ID because GCS does lexicographic
+	sort.Slice(txns, func(i, j int) bool {
+		return txns[i].id < txns[j].id
+	})
+
+	// update the bands
+	for _, t := range txns {
+		if limit, ok := bandRateLimiter[t.band]; ok {
+			limit()
+			// no point in deleting since we're about to just overwrite it anyway
+		}
+
+		obj := h.b.Object(t.key)
+
+		f, err := obj.NewReader(ctx)
+		if err != nil {
+			return fmt.Errorf("unable to get transaction reader: %w", err)
+		}
+		defer f.Close()
+		p, err := ioutil.ReadAll(f)
+		if err != nil {
+			return fmt.Errorf("unable to read from transaction: %w", err)
+		}
+
+		off := h.blockSize*t.band + t.offset
+
+		err = h.writeToBand(p, off)
+		if err != nil {
+			return fmt.Errorf("unable to write transaction to band: %w", err)
+		}
+
+		timeout := time.After(time.Second)
+		bandRateLimiter[t.band] = func() {
+			<-timeout
+		}
+
+		// it's okay if we die after writing above and before deleting below,
+		// since replaying this transaction will result in the same state.
+		err = obj.Delete(ctx)
+		if err != nil {
+			return fmt.Errorf("unable to delete transaction: %w", err)
+		}
+	}
+
+	return nil
+}
+
 func (h *Handle) Close() error {
 	return nil
 }
 
 func (h *Handle) ReadAt(p []byte, offset uint64) error {
+	// for now, always finalize all transactions before reading.
+	// this is sloooooow. we should either:
+	//  - only finalize transactions for this band, or
+	//  - layer transactions on top of the band, if any, returning a view into
+	//    the state we would have if transactions were finalized
+	err := h.finalizeTransactions()
+	if err != nil {
+		return err
+	}
 	ctx := context.Background()
 
 	// determine which band we're reading from
 	idx := offset / h.blockSize
-	src := fmt.Sprintf("%s/bands/%s", h.prefix, strconv.FormatUint(idx, 36))
+	idxStr := strconv.FormatUint(idx, 36)
+	src := fmt.Sprintf("%s/bands/%s", h.prefix, idxStr)
 
 	// adjust offset to account for band
 	off := offset % h.blockSize
@@ -148,16 +326,57 @@ func (h *Handle) ReadAt(p []byte, offset uint64) error {
 	return nil
 }
 
+// WriteAt always writes transactions
 func (h *Handle) WriteAt(p []byte, offset uint64) error {
 	ctx := context.Background()
+
+	// adjust offset to account for band
+	off := offset % h.blockSize
+
+	// start a transaction
+	txn := h.txn
+	h.txn++
+
+	// determine which band this transaction is writing to
+	idx := offset / h.blockSize
+	dest := fmt.Sprintf("%s/txns/%s-%d-%d", h.prefix, strconv.FormatUint(idx, 36), txn, off)
+	f := h.b.Object(dest).NewWriter(ctx)
+
+	// does this write need to wrap?
+	var rem []byte
+	if off+uint64(len(p)) >= h.blockSize {
+		rem = p[h.blockSize-off:]
+		p = p[:h.blockSize-off]
+	}
+
+	_, err := f.Write(p)
+	if err != nil {
+		return fmt.Errorf("unable to write to transaction: %w", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		return fmt.Errorf("unable to close transaction writer: %w", err)
+	}
+
+	if rem != nil {
+		return h.WriteAt(rem, offset+uint64(len(p)))
+	}
+
+	return nil
+}
+
+// offset is from beginning of file
+func (h *Handle) writeToBand(p []byte, offset uint64) error {
+	ctx := context.Background()
+
+	// adjust offset to account for band
+	off := offset % h.blockSize
 
 	// determine which band we're writing to
 	idx := offset / h.blockSize
 	dest := fmt.Sprintf("%s/bands/%s", h.prefix, strconv.FormatUint(idx, 36))
 	f := h.b.Object(dest).NewWriter(ctx)
-
-	// adjust offset to account for band
-	off := offset % h.blockSize
 
 	// does this write need to wrap?
 	var rem []byte
@@ -184,7 +403,7 @@ func (h *Handle) WriteAt(p []byte, offset uint64) error {
 	}
 
 	if rem != nil {
-		return h.WriteAt(rem, offset+uint64(len(p)))
+		return h.writeToBand(rem, offset+uint64(len(p)))
 	}
 
 	return nil
--- backends/gcs/backend_test.go
+++ backends/gcs/backend_test.go
@@ -13,7 +13,7 @@ func TestWriteRead(t *testing.T) {
 		t.Fatalf("unable to create file backend: %v", err)
 	}
 
-	// TODO: defer deleting all of the stuff
+	f.Delete("test-write-read")
 
 	// make sure we conform to the interface
 	var fh cloudblock.Handle
@@ -27,16 +27,16 @@ func TestWriteRead(t *testing.T) {
 	// simple write at the beginning
 	err = fh.WriteAt([]byte("hello"), 0)
 	if err != nil {
-		t.Errorf("unable to write: %v", err)
+		t.Fatalf("unable to write: %v", err)
 	}
 	p := make([]byte, 5)
 	err = fh.ReadAt(p, 0)
 	if err != nil {
-		t.Errorf("unable to read: %v", err)
+		t.Fatalf("unable to read: %v", err)
 	}
 	expected := []byte("hello")
 	if !bytes.Equal(p, expected) {
-		t.Errorf("expected %v, got %v", expected, p)
+		t.Fatalf("expected %v, got %v", expected, p)
 	}
 
 	// write with offset. zeroes should be filled in on either side
@@ -82,5 +82,41 @@ func TestWriteRead(t *testing.T) {
 	expected = []byte("hello")
 	if !bytes.Equal(p, expected) {
 		t.Errorf("expected %v, got %v", expected, p)
+	}
+}
+
+func TestRepeatedWrites(t *testing.T) {
+	f, err := NewBackend("cloudblock-test")
+	if err != nil {
+		t.Fatalf("unable to create file backend: %v", err)
+	}
+
+	f.Delete("test-repeated-writes")
+
+	// make sure we conform to the interface
+	var fh cloudblock.Handle
+
+	// 1 gigabyte, 100 byte bands
+	fh, err = f.New("test-repeated-writes", 1000*1000*1000, 100)
+	if err != nil {
+		t.Fatalf("unable to create handle: %v", err)
+	}
+
+	// simple write at the beginning. if we error out here then we're probably
+	// not respecting GCS rate limits.
+	for i := 0; i < 50; i++ {
+		err = fh.WriteAt([]byte("hello"), 0)
+		if err != nil {
+			t.Fatalf("unable to write: %v", err)
+		}
+		p := make([]byte, 5)
+		err = fh.ReadAt(p, 0)
+		if err != nil {
+			t.Fatalf("unable to read: %v", err)
+		}
+		expected := []byte("hello")
+		if !bytes.Equal(p, expected) {
+			t.Fatalf("expected %v, got %v", expected, p)
+		}
 	}
 }
